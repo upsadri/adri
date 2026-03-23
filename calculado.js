d6e7911f442182d4ed05b4b3a6feebb348fa3be0
Guilhermesenai07 <guilherme.c.santana@ba.estudante.senai.br>
2026-03-23 19:26:52 -0300

    first commit

---
+++ calculado.js
@@ -0,0 +1,30 @@
+let numero1 = 100
+let numero2 = 100
+let operacao = "*"
+
+if (operacao === "+"){
+ let soma = numero1 + numero2
+ console.log ("resultado", soma)
+}
+else if (operacao === "-"){
+    let subtracao = numero1 - numero2
+    console.log ("resultado",subtracao)
+}
+else if (operacao === "*"){
+    let multiplicacao = numero1 * numero2
+    console.log ("resultado", multiplicacao)
+}
+else if (operacao === "/" && numero2 == 0 ){
+    console.log ("erro")
+}
+else if (operacao === "*" && numero1 >100 && numero2 >100 ){
+    console.log ("erro")
+}
+
+else if (operacao === "/"){
+    let divisao = numero1 / numero2
+    console.log ("resultado", divisao)
+}
+else{
+    console.log ("erro operacao")
+}
